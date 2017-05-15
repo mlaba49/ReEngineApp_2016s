2c9485837279864b66105f7caa3e66bfae5a6d3b
MAIN\mjl7864 <mjl7864@rit.edu>
2017-05-15 14:25:15 -0400

    Defeated Practice Exam 4.

--- Ex02_D/MyOctant.cpp
+++ Ex02_D/MyOctant.cpp
@@ -11,15 +11,26 @@ void MyOctant::Init(void)
 	int nObjectCont = m_pBOMngr->GetObjectCount();
 	//YOUR CODE GOES HERE
 	vector3 sum = vector3(0.0f);
+	vector3 min = vector3(0.0f);
+	vector3 max = vector3(0.0f);
 	for (int i = 0; i < nObjectCont; i++) {
 		MyBOClass* temp = m_pBOMngr->GetBoundingObject(i);
 		sum += temp->GetCenterGlobal();
-		//delete temp;
+		if (min.x > temp->GetMinG().x) min.x = temp->GetMinG().x;
+		else if (max.x < temp->GetMaxG().x) max.x = temp->GetMaxG().x;
+		if (min.y > temp->GetMinG().y) min.y = temp->GetMinG().y;
+		else if (max.y < temp->GetMaxG().y) max.y = temp->GetMaxG().y;
+		if (min.z > temp->GetMinG().z) min.z = temp->GetMinG().z;
+		else if (max.z < temp->GetMaxG().z) max.z = temp->GetMaxG().z;
+		temp = nullptr;
 	}
 	m_v3Position = sum / float(nObjectCont);
-	std::cout << m_v3Position.x << ", " << m_v3Position.y << ", " << m_v3Position.z << std::endl;
-	m_pMeshMngr->AddCubeToRenderList(glm::translate(m_v3Position) *
-		glm::scale(vector3(5.0f)), REBLUE, WIRE);
+	m_fSize = max.x - min.x;
+	if (m_fSize < (max.y - min.y)) m_fSize = max.y - min.y;
+	if (m_fSize < (max.z - min.z)) m_fSize = max.z - min.z;
+	m_fSize += 5.0f;
+	
+	Display();
 }
 void MyOctant::Release(void)
 {
